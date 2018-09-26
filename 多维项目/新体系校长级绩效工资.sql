6c06b75c8b42c3404d4d13014bf0e992831ebe30
tangxuke <295659187@qq.com>
2018-09-26 09:22:31 +0800

    111

--- 多维项目/新体系校长级绩效工资.sql
+++ 多维项目/新体系校长级绩效工资.sql
@@ -1,19 +1,19 @@
-select 任课老师
+select 任课老师,OKR_教师级别
 ,MAX(新绩效工资) as 新体系每节课最高绩效
 ,MAX(旧绩效工资) as 旧体系每节课最高绩效
 from
 (
-select 任课老师,课程名称,班别,CONVERT(varchar,考勤日期,112)+' '+考勤时间 as 考勤时间
-,SUM(考勤状态) as 人次
-,SUM(收入) as 收入
-,50+5*SUM(考勤状态)+SUM(收入)*0.15 as 新绩效工资
-,40+(case when 年级 IN ('高一','高二','高三') then 5 else 2.5 end)*SUM(考勤状态)+SUM(收入)*0.1 as 旧绩效工资
-from 月份考勤表
-where 任课老师 in (select 职员姓名
-from 人事字典 
-where isnull(OKR_员工薪级,'')<>''
-and OKR_管理级别 in ('区域校长/项目校长/资深校长','中心主任')
-)
-group by 任课老师,课程名称,班别,CONVERT(varchar,考勤日期,112)+' '+考勤时间,年级
+select a.任课老师,b.OKR_教师级别,a.课程名称,a.班别,CONVERT(varchar,a.考勤日期,112)+' '+a.考勤时间 as 考勤时间
+,sum(a.考勤状态) as 人次
+,sum(a.收入) as 收入
+,isnull(c.达标课时工资,c.基本课时工资)+sum(a.考勤状态)*isnull(c.达标人次工资,c.基本人次工资)+sum(a.收入)*isnull(c.达标收入系数,c.基本收入系数) as 新绩效工资
+,40+(case when a.年级 IN ('高一','高二','高三') then 5 else 2.5 end)*SUM(考勤状态)+SUM(收入)*0.1 as 旧绩效工资
+from 月份考勤表 a
+	inner join 人事字典 b on a.任课老师=b.职员姓名
+	inner join OKR绩效参数表 c on b.OKR_教师级别=c.教师级别
+where isnull(b.OKR_员工薪级,'')<>''
+		and b.OKR_管理级别 in ('区域校长/项目校长/资深校长','中心主任')
+group by a.任课老师,b.OKR_教师级别,课程名称,班别,CONVERT(varchar,考勤日期,112)+' '+考勤时间,a.年级
+,c.达标课时工资,c.基本课时工资,c.达标人次工资,c.基本人次工资,c.达标收入系数,c.基本收入系数
 ) a
-group by 任课老师
+group by 任课老师,OKR_教师级别
